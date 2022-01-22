c09e8ed179d29be4bd34f61914d213600209e22d
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-01-22 12:36:36 -0600

    Add files via upload

--- Tareas/P0/ejemplo0.py
+++ Tareas/P0/ejemplo0.py
@@ -1,15 +1,34 @@
-x=4
-y=-32
-z=65
+a = 5.5
+b = 89.134
+c = -55.5
+x = 4
+y = -32
+z = 65
 
-lista=[x, y, z]
-suma=sum(lista)
-maximo=max(lista)
-minimo=min(lista)
-ecuacion=((suma * maximo) + (minimo**3))
+import numpy as np
+from scipy.stats import describe
+from numpy.random import rand
+
+lista = [x, y, z]
+datos = [a, b, c]
+M = np.matrix([lista, datos])
+suma = sum(lista)
+maximo = max(lista)
+minimo = min(lista)
+ecuacion = ((suma * maximo) + (minimo**3))
+estadistica = describe(lista)
+s = np.arange(1, 100, 5)
+aleatorio = rand(10)
 
 print(lista)
 print(suma)
-print(maximo)
-print(minimo)
 print(ecuacion)
+print(estadistica)
+print(M)
+print(s)
+print(s[-6:])
+print(aleatorio)
+
+while y<=0:
+    print("sumo uno, ahora y es:", y)
+    y+=1
