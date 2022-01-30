b910b814ba2a0afd5652d9e7356cce32f39fb33f
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-01-30 15:08:53 -0600

    Add files via upload
    Se añadió la medición del tamaño de las matrices al código

--- Tareas/P0/Medicion_tiempos.R
+++ Tareas/P0/Medicion_tiempos.R
@@ -1,3 +1,8 @@
+library(pryr)
+
+M = function(n) {k = 2^n; return(matrix(runif(k*k), nrow = k))}
+
 for (n in 8:13) {
-k=2^n; cat(n, k, system.time(matrix(runif(k*k),ncol=k))[3], '\n')
+cat('La matriz', n, 'con', 2^n, 'elementos tarda',
+system.time(M(n))[3], 'segundos en crearse y pesa', object_size(M(n)), 'Gb', '\n')
 }
