17e4d95d72cb162b65381cce8e496d6be2f089bb
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-01-30 15:47:35 -0600

    Add files via upload
    correccion de Gb a bytes

--- Tareas/P0/Medicion_tiempos.R
+++ Tareas/P0/Medicion_tiempos.R
@@ -4,5 +4,5 @@ M = function(n) {k = 2^n; return(matrix(runif(k*k), nrow = k))}
 
 for (n in 8:13) {
 cat('La matriz', n, 'con', 2^n, 'elementos tarda',
-system.time(M(n))[3], 'segundos en crearse y pesa', object_size(M(n)), 'Gb', '\n')
+system.time(M(n))[3], 'segundos en crearse y pesa', object_size(M(n)), 'bytes', '\n')
 }
