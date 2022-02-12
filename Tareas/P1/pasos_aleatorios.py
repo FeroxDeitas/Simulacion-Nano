6c9b052d61564f6a04ba9d934b0b2115090f0018
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-02-12 12:15:45 -0600

    Código completo
    El código ya grafica los 3 grupos de caminatas (100, 1,000 y 10,000 pasos) y los coloca en 5 grupos correspondientes a la dimensión donde se realiza el movimiento. Los ejes están confinados a un rango de -100 a 100 por cuestiones de visibilidad del diagrama caja-bigote.

--- Tareas/P1/pasos_aleatorios.py
+++ Tareas/P1/pasos_aleatorios.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from random import random, randint, getrandbits
 from math import fabs, sqrt
 import matplotlib.pyplot as plt
@@ -8,62 +6,66 @@ import numpy as np
 runs = 30 #replicas
 caminatas = [100, 1000, 10000] #pasos
 results = [] #almacena las dimensiones
-group = [] #almacena grupos de caminatas
 
-for i in range(3):
-    dur = caminatas [i]
+for i in range(3): #itera la cantidad de pasos
+    dur = caminatas[i]
     for dim in range(1, 6): #de una a cinco dimensiones
         mayores = []
-        for rep in range(runs):
+        for rep in range(runs): #corre el experimento 30 veces en cada dimension
             pos = [0] * dim
             mayor = 0
             for paso in range(dur):
                 eje = randint(0, dim - 1)
-                if random() < 0.5:
-                    pos[eje] += 1
+                if pos[eje] > -100 and pos[eje] < 100:
+                    if random() < 0.5:
+                        pos[eje] += 1
+                    else:
+                        pos[eje] -= 1
                 else:
-                    pos[eje] -= 1
+                    if pos[eje] == -100:
+                        pos[eje] += 1
+                    if pos[eje] == 100:
+                        pos[eje] -= 1
                 mayor = max(mayor, sqrt(sum([p**2 for p in pos])))
             mayores.append(mayor)
         results.append(mayores)
-    group.append(results)
-#print(results, '\n')
-#print('results tiene', len(results), 'elementos\n')
-#print(group)
-#print('group tiene', len(group), 'elementos\n')
+
+#separar los resultados en tres grupos de caminatas
+walks_1 = results[0:5]
+walks_2 = results[5:10]
+walks_3 = results[10:15]
+
+#empezar a graficar
 
 ticks = ['1', '2', '3', '4', '5']
 
+#funcion definir los colores de cajas y bigotes
 def set_box_color(bp, color):
     plt.setp(bp['boxes'], color=color)
     plt.setp(bp['whiskers'], color=color)
     plt.setp(bp['caps'], color=color)
-    plt.setp(bp['medians'], color=color)
+    plt.setp(bp['medians'], color='orange')
 
 plt.figure()
 
-bpl = plt.boxplot(group[0], positions=np.array(range(len(group[0])))*3.0-0.4, sym='', widths=0.6)
-bpc = plt.boxplot(group[1], positions=np.array(range(len(group[1])))*3.0, sym='', widths=0.6)
-bpr = plt.boxplot(group[2], positions=np.array(range(len(group[2])))*3.0+0.4, sym='', widths=0.6)
+bpl = plt.boxplot(walks_1, positions=np.array(range(len(walks_1)))*5.0-1.0, sym='', widths=0.8)
+bpc = plt.boxplot(walks_2, positions=np.array(range(len(walks_2)))*5.0, sym='', widths=0.8)
+bpr = plt.boxplot(walks_3, positions=np.array(range(len(walks_3)))*5.0+1.0, sym='', widths=0.8)
 set_box_color(bpl, '#D7191C')
 set_box_color(bpc, '#2CB62C')
 set_box_color(bpr, '#2C7BB6')
 
+#leyenda
 plt.plot([], c='#D7191C', label='100 pasos')
 plt.plot([], c='#2CB62C', label='1000 pasos')
 plt.plot([], c='#2C7BB6', label='10000 pasos')
 plt.legend()
 
-plt.xticks(range(0, len(ticks) * 2, 2), ticks)
-plt.xlim(-2, len(ticks)*2)
+plt.xticks(range(0, len(ticks)*5, 5), ticks)
+plt.xlim(-2, len(ticks)*5)
+plt.title('Distancia Euclideana')
+plt.xlabel('Dimensiones')
+plt.ylabel('Distancia Máxima')
 plt.tight_layout()
-#plt.savefig('boxcompare.png')
+plt.savefig('DistanciaEucl.png')
 plt.show()
-
-#fig, ax = plt.subplots()
-#ax.boxplot(group[1])
-#ax.set_xlabel('Dimension')
-#ax.set_ylabel('Distancia maxima')
-#ax.set_title('Distancia Euclideana')
-#plt.savefig('figuraPy.png')
-#plt.show()
