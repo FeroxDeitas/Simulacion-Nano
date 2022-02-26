2a37091927c306201343362adef1b27175b4bf37
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-02-26 12:16:49 -0600

    Version final
    Se hace un precalentamiento del primer algoritmo para remover ese valor de tiempo. Ya grafica los violiplots de ambas variaciones (algoritmo y orden)

--- Tareas/P3/teoria_colas.py
+++ Tareas/P3/teoria_colas.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 d = 1000
 h = 5000
-replicas = 20
+replicas = 30
 original = [x for x in range(d, h + 1)]
 invertido = original[::-1]
 aleatorio = original.copy()
@@ -45,44 +45,52 @@ def primo_3(n):
     return True
 
 if __name__ == "__main__":
-    resultados = {'Primo 1': [], 'Primo 2': [], 'Primo 3': [], \
-                   'Original': [], 'Invertido': [], 'Aleatorio': []}
+    resultados_1 = {'Algoritmo 1': [], 'Algoritmo 2': [], 'Algoritmo 3': []}              
+    resultados_2 = {'Original': [], 'Invertido': [], 'Aleatorio': []}
     with multiprocessing.Pool(processes = cores-1) as pool:
+        pool.map(primo_1, original)
         for r in range(replicas):
             t = (time()*1000)
             pool.map(primo_1, original)
-            resultados['Primo 1'].append((time()*1000)-t)
+            resultados_1['Algoritmo 1'].append((time()*1000)-t)
             t = (time()*1000)
             pool.map(primo_2, original)
-            resultados['Primo 2'].append((time()*1000)-t)
+            resultados_1['Algoritmo 2'].append((time()*1000)-t)
             t = (time()*1000)
             pool.map(primo_3, original)
-            resultados['Primo 3'].append((time()*1000)-t)
+            resultados_1['Algoritmo 3'].append((time()*1000)-t)
             t = (time()*1000)
             pool.map(primo_3, original)
-            resultados['Original'].append((time()*1000) - t)
+            resultados_2['Original'].append((time()*1000) - t)
             t = (time()*1000)
             pool.map(primo_3, invertido)
-            resultados['Invertido'].append((time()*1000) - t)
+            resultados_2['Invertido'].append((time()*1000) - t)
             t = (time()*1000)
             pool.map(primo_3, aleatorio)
-            resultados['Aleatorio'].append((time()*1000) - t)
-    df = pd.DataFrame(data = resultados)
-    print(df)
-    stat1, p1 = f_oneway(resultados['Primo 1'], resultados['Primo 2'], \
-                         resultados['Primo 3'])
+            resultados_2['Aleatorio'].append((time()*1000) - t)
+    df1 = pd.DataFrame(data = resultados_1)
+    df2 = pd.DataFrame(data = resultados_2)
+    print(df1, '\n', df2)
+    stat1, p1 = f_oneway(resultados_1['Algoritmo 1'],
+                         resultados_1['Algoritmo 2'],
+                         resultados_1['Algoritmo 3'])
     print('Variando algoritmo\n', 'stat=%.3f, p=%.3f' % (stat1, p1))
     if p1 > 0.05:
         print('Estadísticamente no significativa\n')
     else:
         print('Estadísticamente significativa\n')
-    stat2, p2 = f_oneway(resultados['Original'], resultados['Invertido'], \
-                         resultados['Aleatorio'])
+    stat2, p2 = f_oneway(resultados_2['Original'],
+                         resultados_2['Invertido'],
+                         resultados_2['Aleatorio'])
     print('Variando orden de numeros\n', 'stat=%.3f, p=%.3f' % (stat1, p1))
     if p1 > 0.05:
         print('Estadísticamente no significativa\n')
     else:
         print('Estadísticamente significativa\n')
     
-    sns.violinplot(data = df, cut = 0, scale = 'count')
+    sns.violinplot(data = df1, scale='count')
+    plt.savefig('AlgorithmVariation.png')
+    plt.show()
+    sns.violinplot(data = df2, scale='count')
+    plt.savefig('OrderVariation.png')
     plt.show()
