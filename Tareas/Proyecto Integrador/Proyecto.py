906c7a19c34c51f39cd8e3718760da3dc0e10007
FeroxDeitas <97479575+FeroxDeitas@users.noreply.github.com>
2022-05-28 19:41:24 -0500

    Codigo actualizado
    Ya se muestran las diferencias entre datos experimentales y ambos modelos teoricos.

--- Tareas/Proyecto Integrador/Proyecto.py
+++ Tareas/Proyecto Integrador/Proyecto.py
@@ -1,41 +1,89 @@
-from math import sqrt, pi
+from math import pi
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-Fmax = 50
-E = 85
-R = 200
-g = 0.2
+datos = pd.read_csv('DatosReales.txt', delimiter = ' ')
 
-F = np.arange(0, Fmax + 0.5, 0.5).tolist()
+F = (datos.Carga)*(10**-3)#N
+d_r = datos.Deformacion
+E = 85*(10**9) #Pa
+R = 200*(10**-9) #m
+g = 300*(10**-3) #J/m2
 
-hertz = []
-jkr = []
-
+hertz_list = []
+jkr_list = []
+Fround = []
+diffa = []
 
 def a(F):
-    return (((3*R)/(4*E))*(F + 6*g*pi*R + sqrt((12*g*pi*R*F) + (6*g*pi*R)**2)))**(1/3)
+    return (((3*R)/(4*E))*(F + 6*g*pi*R + np.sqrt((12*g*pi*R*F) + (6*g*pi*R)**2)))**(1/3)
 
 def d_hertz(F):
-    return ((9*(F**2))/(16*(E**2)*R))**(1/3)
+    return (((9*(F**2))/(16*(E**2)*R))**(1/3))*(10**9) #nm
 
 def d_jkr(F, a, p1, p2):
-    return ((pi*a)/(2*E))*(p1 + 2*p2)
+    return (((pi*a)/(2*E))*(p1 + 2*p2))*(10**9) #nm
 
 for f in F:
     A = a(f)
     p1 = (2*A*E)/(pi*R)
-    p2 = -1 * sqrt((4*g*E)/(pi*A))
-    hertz.append(d_hertz(f))
-    jkr.append(d_jkr(f, A, p1, p2))
-#print('Lista de deformaciones Hertz:\n', hertz)
-#print('\nLista de deformaciones JKR:', jkr)
+    p2 = -1 * np.sqrt((4*g*E)/(pi*A))
+    hertz = d_hertz(f)
+    jkr = d_jkr(f, A, p1, p2)
+    hertz_list.append(hertz)
+    jkr_list.append(jkr)
+    diffa.append(abs(jkr - hertz))
 
-plt.figure(figsize=(7, 3), dpi=1200)
-plt.plot(F, hertz)
-plt.plot(F, jkr)
+F = datos.Carga
+
+diffb = abs(d_r - hertz_list)
+diffc = abs(d_r - jkr_list)
+
+fig = plt.figure(figsize=(7, 3), dpi=1200)
+ax = plt.subplot(111)
+plt.plot(F, d_r, 'k-', label = 'Experimental')
+plt.plot(F, jkr_list, 'g--', label = 'JKR')
 plt.xlabel('Carga (mN)')
 plt.ylabel('Deformación (nm)')
-plt.savefig('proyecto.png', bbox_inches= 'tight')
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.savefig('pr_e_jkr.png', bbox_inches= 'tight')
+plt.close()
+
+fig = plt.figure(figsize=(7, 3), dpi=1200)
+ax = plt.subplot(111)
+plt.plot(F, d_r, 'k-', label = 'Experimental')
+plt.plot(F, hertz_list, 'r--', label = 'Hertz')
+plt.xlabel('Carga (mN)')
+plt.ylabel('Deformación (nm)')
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.savefig('pr_e_h.png', bbox_inches= 'tight')
+plt.close()
+
+fig = plt.figure(figsize=(7, 3), dpi=1200)
+ax = plt.subplot(111)
+plt.plot(F, diffa, 'g--')
+plt.xlabel('Carga (mN)')
+plt.ylabel('Error entre modelo JKR y Hertz (nm)')
+plt.savefig('diferencias_jkr_hertz.png', bbox_inches = 'tight')
+plt.close()
+
+fig = plt.figure(figsize=(7, 3), dpi=1200)
+ax = plt.subplot(111)
+plt.plot(F, diffb, 'r--')
+plt.xlabel('Carga (mN)')
+plt.ylabel('Error entre datos experimentales y Hertz (nm)')
+plt.savefig('diferencias_e_h.png', bbox_inches = 'tight')
+plt.close()
+
+fig = plt.figure(figsize=(7, 3), dpi=1200)
+ax = plt.subplot(111)
+plt.plot(F, diffc, 'b--', label = 'Experimental - JKR')
+plt.xlabel('Carga (mN)')
+plt.ylabel('Error entre datos experimentales y JKR (nm)')
+plt.savefig('diferencias_e_jkr.png', bbox_inches = 'tight')
 plt.close()
